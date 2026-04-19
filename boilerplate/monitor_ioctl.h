f26d07dd2958ba2c0aafd72553698aceef4c7261
vasanthreddy121 <reddyvk174@gmail.com>
2026-04-19 12:28:16 +0530

    Update monitor_ioctl.h

--- boilerplate/monitor_ioctl.h
+++ boilerplate/monitor_ioctl.h
@@ -1,6 +1,6 @@
 #ifndef MONITOR_IOCTL_H
 #define MONITOR_IOCTL_H
-
+ 
 #ifdef __KERNEL__
 #include <linux/ioctl.h>
 #include <linux/types.h>
@@ -8,18 +8,18 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #endif
-
+ 
 #define MONITOR_NAME_LEN 32
-
+ 
 struct monitor_request {
     pid_t pid;
     unsigned long soft_limit_bytes;
     unsigned long hard_limit_bytes;
     char container_id[MONITOR_NAME_LEN];
 };
-
+ 
 #define MONITOR_MAGIC 'M'
-#define MONITOR_REGISTER _IOW(MONITOR_MAGIC, 1, struct monitor_request)
+#define MONITOR_REGISTER   _IOW(MONITOR_MAGIC, 1, struct monitor_request)
 #define MONITOR_UNREGISTER _IOW(MONITOR_MAGIC, 2, struct monitor_request)
-
+ 
 #endif
